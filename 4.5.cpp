6f0e1af7088a2f883b0c2cc1a3b9ffbfd92c0c96
VicVipeZzz <viktorboris62@gmail.com>
2021-10-21 12:15:00 +0300

    first commit

---
+++ 4.5.cpp
@@ -0,0 +1,40 @@
+// Lab_04_5.cpp 
+// < Борис Віктор > 
+// Лабораторна робота № 4.5 
+// Попадання у плоску фігуру.
+// Варіант 1 
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y, R;
+	srand((unsigned)time(NULL));
+	cout << "R = "; cin >> R;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+
+		if (y >= 0 && x >= 0 && y >= x && R * R >= x * x + y * y || y <= 0 && x <= 0 && y <= x && R * R >= x * x + y * y)
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+	cout << "R = "; cin >> R;
+	for (int i = 0; i < 10; i++)
+	{
+		x = 2 * R * rand() / RAND_MAX - R;
+		y = 2 * R * rand() / RAND_MAX - R;
+		if (y >= 0 && x >= 0 && y >= x && R * R >= x * x + y * y || y <= 0 && x <= 0 && y <= x && R * R >= x * x + y * y)
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+		else
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+	}
+	return 0;
+}
+
